7c0ec520e1d460b8707519297c9314908722066f
André Almeida <almeidacandrec@gmail.com>
2022-05-17 15:13:32 -0300

    Verifica se a senha do tipo de conta desejada está armazenada, se sim, copia para a área de transferência

--- pw.py
+++ pw.py
@@ -5,9 +5,15 @@ PASSWORDS = {'email':'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
              'blog':'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
              'luggage':'12345'}
 
-import sys
+import sys,pyperclip
 if len(sys.argv) < 2:
     print('Usage: python pw.py [account] - copy account password')
     sys.exit()
 
 account = sys.argv[1]   #o primeiro argumento da linha de comando é o nome da conta
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print(f'Password for {account} copied to cliboard')
+else:
+    print(f'There is no account named {account}')
