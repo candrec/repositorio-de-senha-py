804dc32e37495e917107c0c1a93ad2676542818d
André Almeida <almeidacandrec@gmail.com>
2022-05-17 14:51:08 -0300

    add dicionário contendo as senhas

--- pw.py
+++ pw.py
@@ -1,1 +1,6 @@
-#! usr/bin/python
+#! usr/bin/python3
+# pw.py - Um programa para repositório de senhas que não é seguro, apenas para estudo
+
+PASSWORDS = {'email':'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+             'blog':'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+             'luggage':'12345'}
